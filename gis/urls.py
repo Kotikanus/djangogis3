7f11e59a601453dbabff3ee684128a83b3f1d94d
Kotikanus <zheludovkostya@gmail.com>
2019-03-26 00:10:12 +0200

    Article template was created . Get_short_text method was created in model

--- gis/urls.py
+++ gis/urls.py
@@ -13,12 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.conf.urls import url
-from django.contrib import admin
-
 import gis.views
+
 urlpatterns = [
     url(r'^$', gis.views.home, name='home'),
-    url(r'^about/$', gis.views.about, name='about')
+    url(r'^about/$', gis.views.about, name='about'),
+    url(r'^articles/(?P<article_id>[0-9]+)/$', gis.views.show_articles, name='article'),
 ]
--- gis/views.py
+++ gis/views.py
@@ -2,8 +2,7 @@
 from __future__ import unicode_literals
 
 from django.http import HttpResponse
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here
 from gis.models import Articles
@@ -15,9 +14,14 @@ def home(request):
 
     articles = Articles.objects.all()
     context = {
-        'articles':articles
+        'articles': articles
     }
-    return render(request,'gis/home.html',context)
+    return render(request, 'gis/home.html', context)
+
+
+def show_articles(request, article_id):
+    article = get_object_or_404(Articles, id=article_id)
+    return render(request, 'gis/article.html', {'article': article})
 
 
 def about(request):
